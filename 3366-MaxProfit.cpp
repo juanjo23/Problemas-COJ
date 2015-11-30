32328ba492c46b3842ea6ba8353929f881011486
Juanjo <juanjo_23@live.com>
2015-11-30 13:05:17 -0600

    closes 5

---
+++ 3366-MaxProfit.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int shares[101];
+  int n, profit;
+
+  scanf("%d", &n);
+  profit = 0;
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &shares[i] );
+    for(int j = i-1; j>=0; j--){
+      if(shares[i] - shares[j] > profit)
+        profit = shares[i] - shares[j];
+    }
+  }
+  printf("%d\n", profit);
+
+  return 0;
+}
